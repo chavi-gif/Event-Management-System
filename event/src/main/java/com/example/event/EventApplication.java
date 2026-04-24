3ec18c116913eae6436dd36a7d14d5fd0254d75b
Nithya Sooriyapperuma <nithyamaduhansi@gmail.com>
2026-04-24 14:58:04 +0530

    Add Javadoc comment for EventApplication class
    Added a Javadoc comment to describe the main class.

--- event/src/main/java/com/example/event/EventApplication.java
+++ event/src/main/java/com/example/event/EventApplication.java
@@ -3,11 +3,13 @@ package com.example.event;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Entry point for the Event Management System
+ */
 @SpringBootApplication
 public class EventApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EventApplication.class, args);
 	}
-
 }
