26162f45f2eeeaff7d26c3515c4291bd44e4a572
chavi-gif <chavinisha11@gmail.com>
2026-04-26 14:17:45 +0530

    Update Resource.java

--- event/src/main/java/com/example/event/Model/Resource.java
+++ event/src/main/java/com/example/event/Model/Resource.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+
 import java.util.List;
 
 @Entity
